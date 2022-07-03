90304b126e6b398bbe35e26ac8e71a58f3654633
nigani <nigani@internet.ru>
2022-07-03 04:04:18 +0300

    2 files modified, 4 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import "./App.css";
+import './App.css';
+import WidgetBox from './components/WidgetBox.js';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <WidgetBox />
   );
 }
